74c4de6a7d7619a7895c6aa0e1fa1079135cd8ad
Limhagyeong <aamumu88@gmail.com>
2024-09-17 02:08:25 +0900

    JoinPage

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   <HeaderLayout />
+   <div id="content" class="content">
+    <router-view></router-view>
+   </div>
+  </div>
+</template>
+
+<script>
+import HeaderLayout from './components/layout/HearderLayout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderLayout
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,6 @@
+<template>
+mainpage
+</template>
+
+<script>
+</script>
