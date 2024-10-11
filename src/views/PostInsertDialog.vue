b1f81a2199579f26004a0c36a6d7f288eb1260e1
Limhagyeong <aamumu88@gmail.com>
2024-10-11 18:52:15 +0900

    postInsert

--- src/views/PostInsertDialog.vue
+++ src/views/PostInsertDialog.vue
@@ -1,141 +1,129 @@
 <template>
-    <div class="pa-4 text-center">
+    <v-container>
       <v-dialog
         v-model="dialog"
+        max-width="850"
         persistent
-        max-width="600"
       >
   
         <v-card
           prepend-icon="mdi-account"
           title="Upload Post"
+          style="height: 100%;"
         >
-          <v-card-text>
-            <v-row dense>
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
+          <v-card-text style="display: flex; height: 100%;" no-gutters>
+            <v-col cols="12" md="6" sm="6" style="cursor: pointer;" >
+              <div class="imgBox">
+              <v-img 
+               class="selectedImg"
+               :src="defaultImg"
+               @click="selectImage"
+               /> 
+              </div>
+            </v-col> 
+            <v-col cols="12" md="6" sm="6"> 
+              <v-textarea 
+              class="hg_textarea"
+              label="사진에 담긴 경험을 공유해주세요!"
+              hide-details
               >
-                <v-text-field
-                  label="First name*"
-                  required
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
-              >
-                <v-text-field
-                  hint="example of helper text only on focus"
-                  label="Middle name"
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
-              >
-                <v-text-field
-                  hint="example of persistent helper text"
-                  label="Last name*"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
-              >
-                <v-text-field
-                  label="Email*"
-                  required
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
-              >
-                <v-text-field
-                  label="Password*"
-                  type="password"
-                  required
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                md="4"
-                sm="6"
-              >
-                <v-text-field
-                  label="Confirm Password*"
-                  type="password"
-                  required
-                ></v-text-field>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-  
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  auto-select-first
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-  
-            <small class="text-caption text-medium-emphasis">*indicates required field</small>
+              </v-textarea> 
+            </v-col>
           </v-card-text>
+
+          <!-- 파일 입력 -->
+          <v-file-input 
+            ref="imageFile" 
+            v-model="imgFile"
+            accept=".png, .jpg" 
+            style="display: none;"
+            @change="imgChange"
+          />
   
           <v-divider></v-divider>
-  
-          <v-card-actions>
-            <v-spacer></v-spacer>
+
+          <v-card-actions> 
   
             <v-btn
               text="Close"
               variant="plain"
-              @click="dialog = false"
+              @click="dialog=false"
             ></v-btn>
   
             <v-btn
               color="primary"
               text="Save"
               variant="tonal"
-              @click="dialog = false"
+              @click="upload"
             ></v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-    </div>
+    </v-container>
   </template>
 
 <script>
+import api from '@/api';
 export default {
   data: () => ({
     dialog: false,
+    defaultImg: require('@/assets/image.png')
   }),
+  methods:{
+    // 이미지 선택창 오픈
+    selectImage() {
+      this.$refs.imageFile.click();
+    },
+    // 선택된 이미지 미리보기
+    imgChange(event) {
+      const file = event.target.files[0];
+      if(file){
+        this.defaultImg=URL.createObjectURL(file);
+      }
+    },
+    // 게시물 insert
+    async upload() {
+      try {
+        console.log(this.imgFile);
+        const formData = new FormData();
+        formData.append('p_img', this.imgFile);
+        formData.append('p_content', this.content);
+        formData.append('username', this.$store.state.id);
+
+        const res = await api.post('/post', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        if (res.status === 200) {
+          alert('게시물이 업로드되었습니다');
+          this.dialog=false;
+        }
+      } catch (error) {
+        alert(error.response.data.message);
+      }
+    },
+  }
 }
 </script>
+<style>
+.imgBox{
+    width:380px;
+    height:390px;
+    overflow:hidden;
+    display: flex; 
+    align-items: center;
+    justify-content: center;
+}
+.selectedImg{
+    width:100%;
+    height:100%;
+    object-fit:cover;
+}
+.hg_textarea .v-input__control{
+  height: 390px !important;
+  border-radius: 0 !important;
+  resize: none !important;
+}
+</style>
--- src/views/PostingPage.vue
+++ src/views/PostingPage.vue
@@ -18,14 +18,7 @@
           </v-row>
           <!-- 다이얼로그 오픈 -->
           <post-insert-dialog ref="PostInsertDialog" v-model:show="dialog"  :show="hidden"/>
-          <!-- 파일 입력 -->
-          <v-file-input 
-            ref="imageFile" 
-            v-model="imgFile"
-            accept="image/png, image/jpg" 
-            style="display: none;"
-            @change="handleImageChange"
-          />
+          
           <v-row style="width: 380px; margin:20px 0px 0px -10px;" justify="start">
             <span class="mr-4">게시물 0</span>
             <span class="mr-4">팔로워 0</span>
@@ -39,14 +32,10 @@
       <v-divider style="margin-top: 20px; width: 1000px;"></v-divider>
     </v-row>
 
-
-    
-   
   </v-container>
 </template>
 
 <script>
-import api from '@/api';
 import PostInsertDialog from './PostInsertDialog.vue';
 export default {
   data() {
@@ -63,44 +52,7 @@ export default {
   methods: {
     openPostDialog() {
       this.$refs.PostInsertDialog.dialog = true; // 다이얼로그 오픈
-    },
-    selectImage() {
-      this.$refs.imageFile.click();
-    },
-    async upload() {
-      try {
-        console.log(this.imgFile);
-        const formData = new FormData();
-        formData.append('p_img', this.imgFile);
-        formData.append('p_content', this.content);
-        formData.append('username', this.$store.state.id);
-
-        const res = await api.post('/post', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-
-        if (res.status === 200) {
-          alert('파일이 업로드 되었습니다');
-          this.imgFile = '';
-        }
-      } catch (error) {
-        alert(error.response.data.message);
-      }
-    },
-    handleImageChange(event) {
-      const file = event.target.files[0];
-      if (file) {
-        this.imgFile = file;
-      }
-    },
-    actionOne() {
-      alert('프로필 편집 클릭됨');
-    },
-    actionTwo() {
-      alert('설정 클릭됨');
-    },
+    },    
   },
 };
 </script>
@@ -109,15 +61,15 @@ export default {
 .avatar-container {
   position: relative;
   display: inline-block;
-  overflow: hidden; /* 이미지가 원형으로 보이도록 설정 */
-  border-radius: 50%; /* 부모 요소에 border-radius 추가 */
-  width: 150px; /* 너비 설정 */
-  height: 150px; /* 높이 설정 */
+  overflow: hidden; 
+  border-radius: 50%;
+  width: 150px; 
+  height: 150px;
 }
 
 .rounded-avatar {
-  border-radius: 50%; /* 이미지에 border-radius 추가 */
-  width: 100%; /* 너비를 부모 요소에 맞춤 */
-  height: 100%; /* 높이를 부모 요소에 맞춤 */
+  border-radius: 50%; 
+  width: 100%; 
+  height: 100%;
 }
 </style>
